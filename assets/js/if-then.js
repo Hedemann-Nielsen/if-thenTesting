bcfcd5bb9a7884c11886edf5a658eea2665c5da1
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2023-09-04 11:38:41 +0200

    grøn og gul opgave er løst

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -7,32 +7,48 @@ const D = 10;
 const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
+  
 
-if ( /*..dit expression Her*/ ) {
+if (A==B) {
     console.log('statement true!');
 }
 
 
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
-
-
+if (A>B) {
+    console.log('A er større end B!');
+} else {
+    console.log('A er mindre end B');
+} 
 
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
-
+if (A <= B) {
+    console.log('A er mindre eller = end B');
+} else {
+    console.log(' A er større end B');
+}
 
 
 
 
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
-
+if (!(B==C)) {
+    console.log('B og C er IKKE ens');
+} else {
+console.log('B og C er ens');
+}
 
 
 
 
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
 
-
+if (B===C) {
+    console.log('B og C er ens');
+} else {
+console.log('B og C er IKKE ens');
+}
 
 
 //opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
--- assets/js/operators.js
+++ assets/js/operators.js
@@ -5,23 +5,36 @@ const B = 30;
 const C = '30';
 const D = 10;
 const E = 40;
-
+var F;
 // Opgave 1: skriv et expression der viser true når A er lig med B eller A er lig med D.
 
-if ( /*..dit expression Her*/ ) {
-    console.log('statement true!');
+if (A==B || A==D) {
+    console.log('A er lig med B eller A er lig med D');
+}else {
+    console.log('statement false!');
 }
 
 
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
-
+if (A>B && A<E) {
+    console.log('statement true!');
+} else {
+    console.log('statement false!');
+}
 
 
 
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
 
-
+ if ( B==C && A<B || A==D ) {
+    console.log('alle er sande');
+ }
 
 
 
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
+if (!F) {
+    console.log('F findes ikke');
+} else {
+    console.log('F findes');
+}
